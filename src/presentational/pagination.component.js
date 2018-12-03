caee74e2c5c37c2e6a3d14acb81aa9bf31089934
Kamil Szczypior <kszczypior1@gmail.com>
2018-12-03 15:33:47 +0100

    Add styles and buttons in pagination component

--- src/presentational/pagination.component.js
+++ src/presentational/pagination.component.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './pagination.css';
 
 import DevTools from '../DevTools';
 
-const Pagination = props => (    
-    props.pageNumbers.map((number) =>{
-        return (
-            <div>                
-                <li key={number} id={number} onClick={props.handleClick.bind(this)} >
-                    {number}
-                </li>
-            </div>
-        )
-    }) 
+
+const Pagination = props => (
+    <div className="container">
+        <button className="buttons">Prev</button>
+        {props.pageNumbers.map((number) =>{
+            return (                         
+                <li key={number} id={number} onClick={props.handleClick.bind(this)} className="listStyle">
+                        {number}
+                </li>            
+            )
+        }) }
+        <button className="buttons">Next</button>
+    </div>    
+    
 );
 
 export default Pagination;
